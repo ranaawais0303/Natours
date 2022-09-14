b1a66249468998201bb49acee1278adaa60db082
Rana_awais <z.manzoor@pioneerlogics.com>
2022-09-14 02:58:20 -0700

    get request from inside file

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
 /////////get request//////////
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello from server side', app: 'Natours' });
-});
+// app.get('/', (req, res) => {
+//   res.status(200).json({ message: 'Hello from server side', app: 'Natours' });
+// });
 
-////////////Post Request/////
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint..');
+// ////////////Post Request/////
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint..');
+// });
+
+/////////Get api //////////
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tours,
+    },
+  });
 });
 
 const port = 3000;
