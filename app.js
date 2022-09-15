1b31baa65256928654ba715431e4a6fe20500902
Rana_awais <z.manzoor@pioneerlogics.com>
2022-09-15 02:27:19 -0700

    remove comented code

--- app.js
+++ app.js
@@ -6,16 +6,6 @@ const app = express();
 
 app.use(express.json());
 
-/////////get request//////////
-// app.get('/', (req, res) => {
-//   res.status(200).json({ message: 'Hello from server side', app: 'Natours' });
-// });
-
-// ////////////Post Request/////
-// app.post('/', (req, res) => {
-//   res.send('You can post to this endpoint..');
-// });
-
 /////////Get api //////////
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
