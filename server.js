686b5621d60b9b6d9f4d563793805a479eb53621
Rana_awais <z.manzoor@pioneerlogics.com>
2022-09-21 03:01:59 -0700

    send data to mongo db by usind express node

--- server.js
+++ server.js
@@ -18,6 +18,44 @@ mongoose
   .then((con) => {
     console.log(con.connections);
   });
+
+////////Schema for our data/////////
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+//Model
+const Tour = mongoose.model('Tour', tourSchema);
+
+//document
+const testTour = new Tour({
+  name: 'The Forest Hiker',
+  rating: 4.7,
+  price: 497,
+});
+
+//save testTour
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('ERROR 💥 :', err);
+  });
+
 //4) START SERVER
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
