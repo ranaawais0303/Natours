8ec47f7ad9aed0f18802af1022eb28e6aa78434f
Rana_awais <z.manzoor@pioneerlogics.com>
2022-09-20 23:12:46 -0700

    mongoose setup

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
+const mongoose = require('mongoose');
+
 const dotenv = require(`dotenv`);
 const app = require(`./app`);
 
 dotenv.config({ path: 'config.env' });
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+  });
 //4) START SERVER
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`app running on port ${port}...`);
+  console.log('db connection successful');
 });
