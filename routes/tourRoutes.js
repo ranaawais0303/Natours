594e8fc0967ff9cc1daf2e25e5c2a54badda5c88
Rana_awais <z.manzoor@pioneerlogics.com>
2022-09-21 03:54:51 -0700

    set according to eslint

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const tourController = require('./../controllers/tourController');
+
+const tourController = require(`./../controllers/tourController`);
+
 const {
   getAllTours,
   getTour,
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const userController = require('./../controllers/userController');
+const userController = require(`./../controllers/userController`);
 //3) ROUTES
 
 const router = express.Router();
