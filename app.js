9c9ecf1ed94a0157d8e8d412daac5b94aed127f1
Rana_awais <z.manzoor@pioneerlogics.com>
2022-09-29 00:56:29 -0700

    implement global error handling middleware express

--- app.js
+++ app.js
@@ -40,9 +40,23 @@ app.use('/api/v1/users', userRouter);
 
 //unhandled url or route
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'Fail',
-    message: `Can't find ${req.originalUrl} on this server`,
+  // res.status(404).json({
+  //   status: 'Fail',
+  //   message: `Can't find ${req.originalUrl} on this server`,
+  // });
+  const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  err.status = 'fail';
+  err.statusCode = 404;
+  next(err);
+});
+
+//error handler middleware for all error
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
