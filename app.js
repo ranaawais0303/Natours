eb43667b2f8372feec17e9a52e8c18cd494db747
Rana_awais <z.manzoor@pioneerlogics.com>
2022-09-29 00:11:22 -0700

    handling unhandled routes

--- app.js
+++ app.js
@@ -18,10 +18,10 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 ////custom middleware/////
-app.use((req, res, next) => {
-  console.log('Hello from middleware');
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log('Hello from middleware');
+//   next();
+// });
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -37,5 +37,13 @@ app.use((req, res, next) => {
 //Mounting the router
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+//unhandled url or route
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'Fail',
+    message: `Can't find ${req.originalUrl} on this server`,
+  });
+});
 
 module.exports = app;
