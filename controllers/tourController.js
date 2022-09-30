01a35ea4ca9f64d1dfe56339c34f0d603e04fd3f
Rana_awais <z.manzoor@pioneerlogics.com>
2022-09-29 23:52:40 -0700

    handle 404 not found

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,6 @@
 const Tour = require(`./../models/tourModel`);
 const APIFeatures = require('./../utils/apiFeatures');
+const AppError = require('./../utils/appError');
 const catchAsync = require('./../utils/catchAsync');
 
 exports.aliasTopTours = (req, res, next) => {
@@ -39,6 +40,9 @@ exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
   // Tour.findOne({_id:req.params.id}) same
 
+  if (!tour) {
+    return next(new AppError('No Tour found with that ID', 404));
+  }
   res.status(200).json({
     status: 'success',
     data: {
@@ -80,8 +84,11 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 
 //////////Delete Tour//////////
 exports.delelteTour = catchAsync(async (req, res, next) => {
-  await Tour.findByIdAndDelete(req.params.id);
+  const tour = await Tour.findByIdAndDelete(req.params.id);
 
+  if (!tour) {
+    return next(new AppError('No Tour found with that ID', 404));
+  }
   res.status(204).json({
     status: 'success',
     data: null,
