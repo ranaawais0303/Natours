71214324065a251576778dc0e3e9bdae87b7187f
Rana_awais <z.manzoor@pioneerlogics.com>
2022-10-05 02:38:03 -0700

    signup remaining and JWT use

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,28 @@
+const jwt = require('jsonwebtoken');
 const User = require('./../models/userModal');
 const catchAsync = require('./../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  //from environment variable
+  const secret = process.env.JWT_SECRET;
+  const expireTime = process.env.JWT_EXPIRES_IN;
+
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+  const token = jwt.sign(
+    {
+      id: newUser._id,
+    },
+    secret,
+    { expiresIn: expireTime }
+  );
   res.status(201).json({
     status: 'Successs',
+    token,
     data: {
       user: newUser,
     },
