f815059ea2944a46a254bd3974e9a29c7401ec00
Rana_awais <z.manzoor@pioneerlogics.com>
2022-10-12 05:05:30 -0700

    limiter block the extra req that exceede the limit

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('././controllers/errorController');
 
@@ -9,11 +11,18 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 console.log(process.env.NODE_ENV);
-//1) MIDDLEWARES
+//1) GLOBAL MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
   //show logging data into console
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 2,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP, please try again in an hour',
+});
+app.use('/api', limiter);
 
 ////express.json is middleware////
 app.use(express.json());
