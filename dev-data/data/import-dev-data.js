85c9fc222a7458824613c5f1555c32c36d6927df
Rana_awais <z.manzoor@pioneerlogics.com>
2022-10-18 00:19:27 -0700

    modeling locations

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -24,9 +24,7 @@ mongoose
 
 ///Read JSON File
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
-);
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
 
 //import data INTO DATABASE
 const importData = async () => {
